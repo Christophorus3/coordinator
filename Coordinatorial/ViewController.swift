dbfac5e4c465749ed92e42a2cbfb035cc94e5c5f
christophwottawa <christoph.wottawa@me.com>
2019-07-12 16:59:20 +0200

    Initial Commit

---
+++ Coordinatorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinatorial
+//
+//  Created by Christoph Wottawa on 12.07.19.
+//  Copyright © 2019 Christoph Wottawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
